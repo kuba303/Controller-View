30c44e434fe7bb1f9c51b669c7e8d35a372746b6
Jakub Skrzypek <kuba81s@o2.pl>
2020-07-18 10:36:06 +0100

    add body to Controller

--- index.php
+++ index.php
@@ -17,40 +17,10 @@ const DEFAULT_ACTION = 'home';
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
 $controller = new Controller();
-$controller->run();
-dump($controller);
+$controller->run($action);
+
 //imp View
 $view = new View();
 // parametry do wyswietlania
 $viewParams = [];
-switch ($action) {
-    // case 'home':
-    //   $viewParams['mamyDom'] = "udalo sie dom";
-    //   $page = 'home';
-    //   break;
-  case 'option1':
-    $viewParams['opcja1'] = "udalo sie opcja1";
-    $page = 'option1';
-    break;
-  case 'option2':
-    $viewParams['opcja2'] = "udalo sie opcja2";
-    $page = 'option2';
-    break;
-  case 'option3':
-    $viewParams['opcja3'] = "udalo sie opcja3";
-    $page = 'option3';
-    break;
-  case 'option4':
-    $viewParams['opcja4'] = "udalo sie opcja4";
-    $page = 'option4';
-    break;
-
-  default:
-    $viewParams['mamyDom'] = "udalo sie dom";
-    $page = 'home';
-    break;
-}
-
-
-
 $view->render($page, $viewParams);
--- src/Controller.php
+++ src/Controller.php
@@ -8,7 +8,34 @@ require_once("src/utils/debug.php");
 
 class Controller
 {
-  public function run(): void
+  public function run(string $action): void
   {
+    switch ($action) {
+        // case 'home':
+        //   $viewParams['mamyDom'] = "udalo sie dom";
+        //   $page = 'home';
+        //   break;
+      case 'option1':
+        $viewParams['opcja1'] = "udalo sie opcja1";
+        $page = 'option1';
+        break;
+      case 'option2':
+        $viewParams['opcja2'] = "udalo sie opcja2";
+        $page = 'option2';
+        break;
+      case 'option3':
+        $viewParams['opcja3'] = "udalo sie opcja3";
+        $page = 'option3';
+        break;
+      case 'option4':
+        $viewParams['opcja4'] = "udalo sie opcja4";
+        $page = 'option4';
+        break;
+
+      default:
+        $viewParams['mamyDom'] = "udalo sie dom";
+        $page = 'home';
+        break;
+    }
   }
 }
